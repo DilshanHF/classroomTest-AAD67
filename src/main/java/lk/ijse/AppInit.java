d22e39e9f40a4ae21cd42a0d91251850d1a6d2df
DILSHAN <dilshanfonseka76@gmail.com>
2024-08-31 10:34:35 +0530

    Bens are defined

--- src/main/java/lk/ijse/AppInit.java
+++ src/main/java/lk/ijse/AppInit.java
@@ -1,5 +1,15 @@
 package lk.ijse;
 
-public class AppInit {
+import lk.ijse.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class AppInit {
+    public static void main(String[] args) {
+        var ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+
+
+    }
 }
--- src/main/java/lk/ijse/config/AppConfig.java
+++ src/main/java/lk/ijse/config/AppConfig.java
@@ -1,5 +1,12 @@
 package lk.ijse.config;
 
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = AppConfig.class)
 public class AppConfig {
+
 
 }
